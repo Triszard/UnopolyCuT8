d94b27145983e03ffde89afbfdcb5a601a3cb099
Tristan Klein <tristan.klein@outlook.de>
2022-12-06 19:11:24 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,227 @@
+let SpielFiguren: Image[][] = []
+let SpielKarten: Image[][] = []
+let Punkte: number[] = []
 let AugenZahlWürfel: number[] = []
-function WürfelFunktion () {
+function ladeSpielDateien () {
+    SpielFiguren = [[
+    images.createImage(`
+        . . . . .
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `),
+    images.createImage(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . . . . .
+        # # # # #
+        `),
+    images.createImage(`
+        . # . # .
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `),
+    images.createImage(`
+        . . . . .
+        # # . # #
+        . . . . .
+        . # # # .
+        . . . . .
+        `),
+    images.createImage(`
+        # # . # #
+        # . # . #
+        . # . # .
+        . . . . .
+        # # # # #
+        `),
+    images.createImage(`
+        . # . # .
+        # . . . #
+        . . # . .
+        # # . # #
+        . . . . .
+        `)
+    ], [
+    images.createImage(`
+        . # # # .
+        # # # # #
+        . # # # .
+        . . . . .
+        . # # # .
+        `),
+    images.createImage(`
+        . # # # .
+        . # # # .
+        # # # # #
+        . . . . .
+        # # # # #
+        `),
+    images.createImage(`
+        # # # # #
+        # # . # #
+        . . # . .
+        . # # # .
+        . # # # .
+        `),
+    images.createImage(`
+        . # . # .
+        . # # # .
+        # # # # #
+        . # . # .
+        . # . # .
+        `),
+    images.createImage(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        # . # . #
+        `),
+    images.createImage(`
+        # # # # #
+        # . # . #
+        # # # # #
+        # . # . #
+        # # # # #
+        `)
+    ]]
+    SpielKarten = [[
+    images.createImage(`
+        . # . . .
+        # # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        `),
+    images.createImage(`
+        . # . . .
+        # . # . .
+        . . # . .
+        . # . . .
+        # # # . .
+        `),
+    images.createImage(`
+        # # . . .
+        . . # . .
+        # # . . .
+        . . # . .
+        # # . . .
+        `),
+    images.createImage(`
+        # . # . .
+        # . # . .
+        # # # . .
+        . . # . .
+        . . # . .
+        `),
+    images.createImage(`
+        # # # . .
+        # . . . .
+        # # # . .
+        . . # . .
+        # # # . .
+        `),
+    images.createImage(`
+        # # # . .
+        # . . . .
+        # # # . .
+        # . # . .
+        # # # . .
+        `),
+    images.createImage(`
+        # # # . .
+        . . # . .
+        . # . . .
+        # . . . .
+        # . . . .
+        `),
+    images.createImage(`
+        # # # . .
+        # . # . .
+        # # # . .
+        # . # . .
+        # # # . .
+        `),
+    images.createImage(`
+        # # # . .
+        # . # . .
+        # # # . .
+        . . # . .
+        # # # . .
+        `),
+    images.createBigImage(`
+        . # . # # # . . . .
+        # # . # . # . . . .
+        . # . # . # . . . .
+        . # . # . # . . . .
+        . # . # # # . . . .
+        `),
+    images.createImage(`
+        . # . . #
+        # # . # #
+        . # . . #
+        . # . . #
+        . # . . #
+        `),
+    images.createBigImage(`
+        . # . . # . . . . .
+        # # . # . # . . . .
+        . # . . . # . . . .
+        . # . . # . . . . .
+        . # . # # # . . . .
+        `)
+    ], [
+    images.createImage(`
+        . # # # .
+        # . . . #
+        . . . # #
+        . . # . .
+        . . # . .
+        `),
+    images.createImage(`
+        . # # # .
+        # . . . #
+        . . . # #
+        . . # . .
+        . . # . .
+        `),
+    images.createImage(`
+        # # # # #
+        . # # # .
+        . # # # .
+        . . . . .
+        . . # . .
+        `),
+    images.createImage(`
+        # # # # #
+        . # # # .
+        . # # # .
+        . . . . .
+        . . # . .
+        `)
+    ], [images.createImage(`
+        . # # # .
+        # # # # #
+        . # . # .
+        . # # # .
+        . # # # .
+        `), images.createImage(`
+        # # . # #
+        . # . # .
+        . . # . .
+        . # . # .
+        # # . # #
+        `)]]
+    Punkte = [0, 0]
+}
+function wuerfelFunktion () {
     AugenZahlWürfel = [randint(1, 6), randint(1, 6), randint(1, 6)]
-    basic.showNumber(AugenZahlWürfel)
+    basic.showNumber(AugenZahlWürfel[0] + (AugenZahlWürfel[1] + AugenZahlWürfel[2]))
 }
 basic.forever(function () {
 	
