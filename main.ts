0c16b2d25904a6e9b78be5dafd02831947af4cee
Tristan Klein <tristan.klein@outlook.de>
2022-12-06 22:38:59 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ let SpielFiguren: Image[][] = []
 let SpielKarten: Image[][] = []
 let Punkte: number[] = []
 let AugenZahlWürfel: number[] = []
+let Temp: any = null
+let TempErgebnis: Image = null
 function ladeSpielDateien () {
     SpielFiguren = [[
     images.createImage(`
@@ -223,6 +225,11 @@ function wuerfelFunktion () {
     AugenZahlWürfel = [randint(1, 6), randint(1, 6), randint(1, 6)]
     basic.showNumber(AugenZahlWürfel[0] + (AugenZahlWürfel[1] + AugenZahlWürfel[2]))
 }
+function MultiDimArrayReader (num: number, num2: number, list: any[]) {
+    Temp = list[num]
+    TempErgebnis = Temp[num2]
+    return TempErgebnis
+}
 basic.forever(function () {
-	
+    MultiDimArrayReader(1, 1, SpielKarten).showImage(0)
 })
