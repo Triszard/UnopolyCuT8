670cbdf7e0036e964c044495783c9ea843280866
Tristan Klein <tristan.klein@outlook.de>
2022-12-06 08:11:54 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let AugenZahlWürfel: number[] = []
 function WürfelFunktion () {
     AugenZahlWürfel = [randint(1, 6), randint(1, 6), randint(1, 6)]
+    basic.showNumber(AugenZahlWürfel)
 }
 basic.forever(function () {
 	
