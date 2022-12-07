cee788a1f24c0edd644047fb172999898297c43c
Tristan Klein <tristan.klein@outlook.de>
2022-12-07 17:21:41 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
-function CharakterAuswahl (liste: any[]) {
-	
+function AuswahlSystem () {
+    Temp = 0
+    Temp2 = []
+    basic.showString("Such dir einen Charakter aus!", 60)
+    basic.clearScreen()
+    while (true) {
+        SpielFigurenSymbole[Temp].showImage(0, 75)
+        if (input.buttonIsPressed(Button.A) && !(input.buttonIsPressed(Button.B))) {
+            Temp += -1
+        }
+        if (input.buttonIsPressed(Button.B) && !(input.buttonIsPressed(Button.A))) {
+            Temp += 1
+        }
+        if (Temp < 0) {
+            Temp = 5
+        }
+        if (Temp > 5) {
+            Temp = 0
+        }
+        if (input.buttonIsPressed(Button.AB)) {
+            Temp2.push(SpielFigurenSymbole[Temp])
+            break;
+        }
+    }
+    basic.clearScreen()
+    basic.showString("Dein Charakter ist: ", 60)
+    Temp2[0].showImage(0)
+    Temp2.push(SpielFigurenSymbole[randint(6, 11)])
+    basic.showString("Der Computer Charakter ist: ", 60)
+    Temp2[1].showImage(0)
+    basic.clearScreen()
+    return Temp2
 }
 function ladeSpielDateien () {
-    SpielFiguren = [
+    SpielFigurenSymbole = [
     images.createImage(`
         . . . . .
         # # . # #
@@ -88,7 +118,7 @@ function ladeSpielDateien () {
         # . . . #
         `)
     ]
-    SpielKarten = [
+    SpielKartenSymbole = [
     images.createImage(`
         . # . . .
         # # . . .
@@ -224,10 +254,12 @@ function wuerfelFunktion () {
 }
 let AugenZahlWürfel: number[] = []
 let Punkte: number[] = []
-let SpielFiguren: Image[] = []
-let SpielKarten: Image[] = []
+let SpielKartenSymbole: Image[] = []
+let SpielFigurenSymbole: Image[] = []
+let Temp2: Image[] = []
+let Temp = 0
 ladeSpielDateien()
-CharakterAuswahl(SpielKarten)
+let Charaktere = AuswahlSystem()
 basic.forever(function () {
 	
 })
